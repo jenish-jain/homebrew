93d4ed4fa892d171269c9ac0062e85be61939cd7
jenish-jain <jenishjain@rocketmail.com>
2021-08-28 12:11:35 +0530

    updating version

--- sidekick.rb
+++ sidekick.rb
@@ -4,7 +4,7 @@
 class Sidekick < Formula
     desc "Oh, that's my dumb friend"
     homepage "https://github.com/jenish-jain/sidekick"
-    version "1.8.0"
+    version "1.0.1"
     bottle :unneeded
     url "https://github.com/jenish-jain/sidekick/archive/refs/tags/v1.0.1.tar.gz"
     sha256 "769605df3e9bb94a7821fc5d34b55905960b48f4fa0e2e8b959b6a7e5efe1fc6"
