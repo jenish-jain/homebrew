92ccf350e00dbb12ee184b2fe20c9b19abad25ed
jenish-jain <jenishjain@rocketmail.com>
2023-11-04 20:18:53 +0530

    updating sidekick version and moving everything to forumla folder

--- Formula/sidekick.rb
+++ Formula/sidekick.rb
@@ -4,10 +4,10 @@
 class Sidekick < Formula
     desc "Oh, that's my dumb friend"
     homepage "https://github.com/jenish-jain/sidekick"
-    version "1.0.1"
+    version "2.0.0"
     bottle :unneeded
-    url "https://github.com/jenish-jain/sidekick/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "769605df3e9bb94a7821fc5d34b55905960b48f4fa0e2e8b959b6a7e5efe1fc6"
+    url "https://github.com/jenish-jain/sidekick/archive/refs/tags/v2.0.0.tar.gz"
+    sha256 "d64a6e6ba5263a9c7fb2b63bc8e5a145237669f3"
     license "Apache-2.0"
   
     # depends_on "cmake" => :build
